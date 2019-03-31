7289770203e0c48591f276bfff84fde7003222c1
tc1236231 <tc1236231@tc1236231deMacBook-Pro.local>
2019-03-31 14:18:46 -0500

    fix reset password import

--- PilotSystem/app/Notifications/CustomResetPassword.php
+++ PilotSystem/app/Notifications/CustomResetPassword.php
@@ -7,6 +7,7 @@ use Illuminate\Bus\Queueable;
 use Illuminate\Notifications\Notification;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Notifications\Messages\MailMessage;
+use Illuminate\Support\Facades\Lang;
 
 class CustomResetPassword extends ResetPassword
 {
