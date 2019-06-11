0e3458ecc470fd27749e12ceab9b372a10724542
tc1236231 <314368614@qq.com>
2019-06-11 20:04:00 +0800

    added mini mode

--- PilotSystem/resources/views/clientui/topbar.blade.php
+++ PilotSystem/resources/views/clientui/topbar.blade.php
@@ -2,6 +2,7 @@
     <div class="wrapper">
         <div class="userNav">
             <ul>
+                <li><a onclick="onMiniModeBtnClicked()"><img src="{{asset('assets/images/client/cfr/goFull.png')}}"/><span>迷你</span></a></li>
                 <li><a onclick="onMinimizeBtnClicked()"><img src="{{asset('assets/images/client/cfr/goBack.png')}}" /><span>最小</span></a></li>
                 <li><a id="exit" onclick="onExitBtnClicked()"><img src="{{asset('assets/images/client/cfr/deleteFile.png')}}"/><span>退出</span></a></li>
             </ul>
