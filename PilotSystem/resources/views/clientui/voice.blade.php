f9e171f78791557a68e19d8fe6023407e054832d
tc1236231 <314368614@qq.com>
2019-06-21 13:58:47 +0800

    added voice info

--- PilotSystem/resources/views/clientui/voice.blade.php
+++ PilotSystem/resources/views/clientui/voice.blade.php
@@ -8,12 +8,20 @@
         <div class="title"><img src="{{asset('assets/images/client/icons/dark/imageList.png')}}" alt="" class="titleIcon" /><h6>语音设置 | Voice</h6></div>
 
         <div class="wrapper statsItems"><!-- 居中 -->
+            <div class="formRow">
+                <h4 style="margin-bottom: 10px;">本地机模语音组件数据</h4>
+                <div id="localVoiceInfo">
+                    暂无
+                </div>
+            </div>
+            <br />
             <div class="formRow"><h4><a id="PTTKeyLabel" title="" class="value"></a></h4></div><br />
             <a class="button greenB" onclick="configurePTTKey()" style="margin: 5px;"><span>自定义通话键</span></a>
-            <a class="button blackB" onclick="testVoice()" ><span>语音测试开关</span></a> <br /> <br />
-            自定义通话键：单机自定义按钮 → 操作（键盘、摇杆）上的通话按键。  <br /><br />
-            语音测试开关，测试本地通话，再次单击关闭测试语音； <br /><br />
-            断开语音、或被管制踢出语音后，需要重新连线，加入语音通信； <br /><br />
+            <a class="button blackB" onclick="testVoice()" ><span>语音测试开关</span></a> <br />
+            <h4 style="color: red">使用说明</h4>
+            自定义通话键：单机自定义按钮 → 操作（键盘、摇杆）上的通话按键。  <br />
+            语音测试开关，测试本地通话，再次单击关闭测试语音<br />
+            断开语音、或被管制踢出语音后，需要重新连线，加入语音通信； <br />
         </div>
         <div class="line"></div><!-- 分割 --><br />
         <frame>
